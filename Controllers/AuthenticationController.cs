49bea4af8f695503d8b67641feec519931f22275
Bstijn <baltessen.stijn@gmail.com>
2020-04-01 11:56:17 +0200

    Add project files.

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        
+        [HttpPost]
+        public bool Register(string username, string password)
+        {
+            try
+            {
+                //logic
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Interfaces/IAuthenticationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Interfaces
+{
+    public interface IAuthenticationRepository
+    {
+        void RegisterAccount(string username, string password);
+        void Login(string username, string password);
+        void ValidateSession(Object sessionToken);
+    }
+}
